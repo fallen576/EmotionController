c510786f6d1e871afbcfce54c9b7cb961d472c84
Ben Gianni <bgianni@bens-mbp.lan>
2021-12-30 11:15:07 -0500

    init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿const video = $('#video');
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/js'),//tiny_face_detector_model-weights_manifest.json
+  faceapi.nets.faceLandmark68Net.loadFromUri('/js/faceapi/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/js/faceapi/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/js/faceapi/models')
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
